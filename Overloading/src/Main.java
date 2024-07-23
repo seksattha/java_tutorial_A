e7cbbc31941ab79a9557a6f636290045af6eb842
seksatta_HP <seksattha@gmail.com>
2024-07-23 13:25:54 +0700

    demo of overloading method

---
+++ Overloading/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Opertor o1 = new Opertor();
+        System.out.println(o1.add(3, 5));
+        System.out.println(o1.add(3, 6, 10));
+
+
+    }
+}
---
+++ Overloading/src/Opertor.java
@@ -0,0 +1,10 @@
+public class Opertor {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
