a7c1558a7981e7df83edf757ef0b1467e0f732c6
seksatta(labtop) <seksattha@gmail.com>
2024-10-08 10:04:21 +0700

    V6 Adding enum

--- src/InstanceVariables/Model/OliveName.java
+++ src/InstanceVariables/Model/OliveName.java
@@ -1,6 +1,23 @@
 package InstanceVariables.Model;
 
 public enum OliveName {
-    KALAMATA, LIGURIAN;
+    KALAMATA("Katamala"), LIGURIAN("Ligurian");
 
+    //create variable
+    private String name;
+
+    // create a construction method for this enumeration
+
+    // do not put the "Public" key word on this ?
+    //------can be called only in Enum------------
+    OliveName(String name) {
+        this.name = name;
+    }
+
+    // use toString method to show the information.
+
+    @Override
+    public String toString() {
+        return ("Name of Olive =>  " + name);
+    }
 }
