1a6c1dee9e62087d4e52bb8cf269a12031f59f8b
seksatta(labtop) <seksattha@gmail.com>
2024-10-08 09:47:00 +0700

    V4

--- src/InstanceVariables/Main.java
+++ src/InstanceVariables/Main.java
@@ -11,14 +11,10 @@ public class Main {
     public static void main(String[] args) {
         List<Olive> olives = new ArrayList<>();
 
-        Olive olive1 = new Olive("Kalamata", 0x2E0854, 3);
-        olives.add(olive1);
 
-        Olive olive2 = new Olive("Kalamata", 0x2E0854, 3);
-        olives.add(olive2);
-
-        Olive olive3 = new Olive("Ligurian", 0x000000, 3);
-        olives.add(olive3);
+        olives.add(new Olive("Kalamata", 0x2E0854, 3));
+        olives.add(new Olive("Kalamata", 0x2E0854, 3));
+        olives.add(new Olive("Ligurian", 0x000000, 3));
 
         OlivePress press = new OlivePress();
         int totalOil = press.getOil(olives);
