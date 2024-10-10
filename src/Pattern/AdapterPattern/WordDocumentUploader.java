deae93fe9351e6c43bbc9d23d163d23d0dfeedde
seksatta(labtop) <seksattha@gmail.com>
2024-10-10 15:59:49 +0700

    V4 remove ...

--- src/Pattern/AdapterPattern/WordDocumentUploader.java
+++ src/Pattern/AdapterPattern/WordDocumentUploader.java
@@ -2,7 +2,7 @@ package Pattern.AdapterPattern;
 
 public class WordDocumentUploader {
     public void uploadWordDocument() {
-        System.out.println("Preparing Word document ... ");
+        System.out.println("Preparing Word document");
         System.out.println("Uploading Word document");
 
     }
