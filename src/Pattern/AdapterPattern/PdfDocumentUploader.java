4298f9be146048d64219ad7d72e2eb78389e39db
seksatta(labtop) <seksattha@gmail.com>
2024-10-10 15:15:37 +0700

    V2: PDF document uploader

---
+++ src/Pattern/AdapterPattern/PdfDocumentUploader.java
@@ -0,0 +1,9 @@
+package Pattern.AdapterPattern;
+
+public class PdfDocumentUploader implements DocumentUploader{
+    @Override
+    public void upload() {
+        System.out.println("Preparing PDF document ... ");
+        System.out.println("Uploading PDF document");
+    }
+}
